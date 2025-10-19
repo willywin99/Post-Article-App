61bb10ec667ce385a7353c269d05bb440e184564
willywin99 <willywinsianturi@gmail.com>
2025-10-19 09:02:33 +0700

    fix

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { host: true }
+  server: {
+    proxy: {
+      '/article': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  }
 })
